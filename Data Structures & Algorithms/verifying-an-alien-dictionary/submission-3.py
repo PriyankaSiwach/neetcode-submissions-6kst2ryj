f6c30cc2be50a288ee614b06786f853707a75c3e
Priyanka Fnu <167464089+PriyankaSiwach@users.noreply.github.com>
2026-08-17 20:11:48 -0400

    Add: verifying-an-alien-dictionary - submission-3

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        count= {c:i for i,c in enumerate(order)}
+        for i in range(len(words)-1):
+            w1= words[i]
+            w2=words[i+1]
+            for j in range(len(w1)):
+                if j==len(w2):
+                    return False
+                if w1[j]!=w2[j]:
+                    if count[w1[j]]>count[w2[j]]:
+                        return False
+                    break
+        return True
+
+
+
+        
