5fc59060cdf36236eebc3bdd76d16d115c9c4fb3
Priyanka Fnu <167464089+PriyankaSiwach@users.noreply.github.com>
2026-09-19 17:24:45 -0400

    Add: restore-ip-addresses - submission-2

---
+++ Data Structures & Algorithms/restore-ip-addresses/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res=[]
+        path=[]
+        def backtrack(i):
+            if len(path)==4 and i==len(s):
+                res.append(".".join(path)) 
+                return
+            for j in range(i,min(i+3,len(s))):
+                part=s[i:j+1]
+                if int(part)>255 or (len(part)>1 and part[0]=="0" ):
+                    break
+                path.append(part)
+                backtrack(j+1)
+                path.pop()
+        backtrack(0)
+        return res
+
