992ba65366a816aed21ca0f4a79001cc5b853073
Priyanka Fnu <167464089+PriyankaSiwach@users.noreply.github.com>
2026-09-22 21:53:31 -0400

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l,r=0,len(s)-1
+        while l<r:
+            if s[l].lower()!=s[r].lower():
+                skipleft= s[l+1:r+1]
+                skipright= s[l:r]
+                return skipleft==skipleft[::-1] or skipright==skipright[::-1]
+            l+=1
+            r-=1
+        return True
+                
+            
+                
